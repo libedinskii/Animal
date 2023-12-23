fe329942a1db8afbc6a50b5ad3f3b31ef727fd29
libedinskii <124511733+libedinskii@users.noreply.github.com>
2023-12-23 19:18:14 +0900

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Animal animal1 = new Dog();
+        Animal animal2 = new Cat();
+
+        animal1.makeSound(); // Output: The dog barks
+        animal2.makeSound(); // Output: The cat meows
+    }
+}
